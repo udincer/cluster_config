5feef370faf28457b52c890968b176c3e585a7cf
Tevfik U. Dincer <udincer@users.noreply.github.com>
2019-10-07 14:14:53 -0700

    added ucsc tools + bedops

--- cluster_config.sh
+++ cluster_config.sh
@@ -1,7 +1,5 @@
 ## setting up from scratch on the cluster
 
-# don't forget to qrsh for faster installation
-
 # install conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
@@ -10,6 +8,8 @@ conda install -y -c conda-forge tmux
 
 conda install -y numpy scipy pandas scikit-learn seaborn tqdm jupyter statsmodels yapf cython joblib
 conda install -y -c conda-forge jupyter_contrib_nbextensions
+
+conda install -y -c bioconda bedops
 
 # set up jupyter
 jupyter notebook --generate-config
@@ -25,3 +25,7 @@ EOT
 
 echo "alias jn='. ~/utils/start_jupyter.sh'" >> ~/.bashrc
 echo "alias t='tmux attach'" >> ~/.bashrc
+
+# optional, install ucsc utils
+# not using this link might results in link errors to zlib
+# rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64.v369/ ~/ucsc_utils/
