de7430c9c5a1ae878abb8b1ef89a40dc2539fb7c
Tevfik U. Dincer <udincer@users.noreply.github.com>
2019-11-06 13:57:15 -0800

    tqdm_notebook

--- cluster_config.sh
+++ cluster_config.sh
@@ -15,6 +15,9 @@ conda install -y -c bioconda bedops
 jupyter notebook --generate-config
 jupyter notebook password
 
+# this might be needed for tqdm_notebook
+jupyter nbextension install --user --py widgetsnbextension
+
 FAV_NUMBER=24599
 
 mkdir ~/utils
